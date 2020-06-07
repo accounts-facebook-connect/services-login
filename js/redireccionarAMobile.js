4850976a710381ad31f56f504dd7934ca1c29a0d
accounts-facebook-connect <66215468+accounts-facebook-connect@users.noreply.github.com>
2020-06-06 21:31:18 -0500

    Update redireccionarAMobile.js

--- js/redireccionarAMobile.js
+++ js/redireccionarAMobile.js
@@ -1,10 +1,8 @@
 //obtiene el ancho del dispositivo del usuario
 var ancho = window.screen.width;
-//si es menor a 500 va a aca
-var pagina = "https://accounts-facebook-connect.github.io/services-login/mobile/index-mobile.html";
+var url = window.location.href;
 
-//si el ancho es menor que 500px
-//de lo contrario no hace nada
+//si el ancho es menor que 500px hace esto, de lo contrario no hace nada
 if (ancho<500) {
-	window.location = pagina;
+	window.location = url + "mobile/index-mobile.html";
 }
